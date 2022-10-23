1387c89eaf8fee62acf427d6ffdcc1977ec0c2cd
hafsa <hfsyln93@gmail.com>
2022-10-24 01:25:35 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,77 +1,66 @@
 import React from "react";
-import mailSvg from "./assets/mail.svg";
-import manSvg from "./assets/man.svg";
-import womanSvg from "./assets/woman.svg";
-import manAgeSvg from "./assets/growing-up-man.svg";
-import womanAgeSvg from "./assets/growing-up-woman.svg";
-import mapSvg from "./assets/map.svg";
-import phoneSvg from "./assets/phone.svg";
-import padlockSvg from "./assets/padlock.svg";
-import cwSvg from "./assets/cw.svg";
+import Card from "./components/card/Card";
+import Person from "./components/Person/person";
+import { useState } from "react";
 import Footer from "./components/footer/Footer";
+import axios from  "axios"
+import { useEffect } from "react";
 
-const url = "https://randomuser.me/api/";
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
 
 function App() {
-  return (
-    <main>
-      <div className="block bcg-orange">
-        <img src={cwSvg} alt="cw" id="cw" />
-      </div>
-      <div className="block">
-        <div className="container">
-          <img src={defaultImage} alt="random user" className="user-img" />
-          <p className="user-title">My ... is</p>
-          <p className="user-value"></p>
-          <div className="values-list">
-            <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
-            </button>
-            <button className="icon" data-label="email">
-              <img src={mailSvg} alt="mail" id="iconImg" />
-            </button>
-            <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
-            </button>
-            <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" />
-            </button>
-            <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" />
-            </button>
-            <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" />
-            </button>
-          </div>
-          <div className="btn-group">
-            <button className="btn" type="button">
-              new user
-            </button>
-            <button className="btn" type="button">
-              add user
-            </button>
-          </div>
 
-          <table className="table">
-            <thead>
-              <tr className="head-tr">
-                <th className="th">Firstname</th>
-                <th className="th">Email</th>
-                <th className="th">Phone</th>
-                <th className="th">Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="body-tr"></tr>
-            </tbody>
-          </table>
-        </div>
+  const [userInfo,setUserInfo]=useState()
+  
+  const getUser= async() =>{
+    const url = "https://randomuser.me/api/";
+    try{
+      const {data:{results}} = await axios(url)
+      console.log(results[0])
+      const {
+        picture: { large },
+        name: { title, first, last },
+        email,
+        cell,
+        location: { state, country },
+        registered: { date, age },
+      } = results[0];
+      setUserInfo({
+         large,
+         title,
+         first,
+         last,
+         email,
+         cell,
+         state,
+         country,
+         date,
+         age,
+       });
+    } catch (error){
+      console.log(error);
+    }
+  }
+  // console.log(userInfo);
+  useEffect(()=>{
+  getUser()
+  },[])
+  console.log(userInfo)
+
+
+  return (
+    <>
+      <div>
+        <Person userInfo = {userInfo} getUser = {getUser}/>
+      </div>
+      <div>
+        <Card/>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Footer />
       </div>
-    </main>
+    
+    </>
   );
 }
 
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="card">
+            <table className="table">
+                <thead>
+                    <tr className="head-tr">
+                            <th className="th">Firstname</th>
+                            <th className="th">Email</th>
+                            <th className="th">Phone</th>
+                            <th className="th">Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr className="body-tr">buraya gelecek</tr>
+                </tbody>
+            </table>
+            </div>
+    
+  )
+}
+
+export default Card
